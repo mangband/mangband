294aad1b0ae8b0f53798322446db4381d5b3636f
jug <jug@802d84f3-b497-254e-bede-06c0a5a3456a>
2007-12-01 13:01:00 +0000

    Second thoughts, it's probably better to have this work occasionally for someone, rather than have it never work for anyone. Addresses #81.
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@141 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/server/monster2.c
+++ src/server/monster2.c
@@ -172,6 +172,9 @@ void compact_monsters(int size)
 			/* Hack -- High level monsters start out "immune" */
 			if (r_ptr->level > cur_lev) continue;
 
+			/* Ignore nearby monsters */
+			if ((cur_dis > 0) && (m_ptr->cdis < cur_dis)) continue;
+
 			/* Saving throw chance */
 			chance = 90;
 
