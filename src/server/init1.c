00a23488bfb670b2fa953e1424d74b54d7ffa278
flambard <flambard@802d84f3-b497-254e-bede-06c0a5a3456a>
2008-01-22 19:02:44 +0000

    Addresses #223 - Moved grab_one_flag upper in file to please GCC (introduced in [325])
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@328 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/server/init1.c
+++ src/server/init1.c
@@ -1100,6 +1100,26 @@ errr init_v_info_txt(FILE *fp, char *buf)
 	return (0);
 }
 
+/*
+ * Grab one flag from a textual string
+ */
+static errr grab_one_flag(u32b *flags, cptr names[], cptr what)
+{
+	int i;
+
+	/* Check flags */
+	for (i = 0; i < 32; i++)
+	{
+		if (streq(what, names[i]))
+		{
+			*flags |= (1L << i);
+			return (0);
+		}
+	}
+
+	return (-1);
+}
+
 
 
 /*
@@ -1713,25 +1733,6 @@ errr init_f_info_txt(FILE *fp, char *buf)
 
 
 
-/*
- * Grab one flag from a textual string
- */
-static errr grab_one_flag(u32b *flags, cptr names[], cptr what)
-{
-	int i;
-
-	/* Check flags */
-	for (i = 0; i < 32; i++)
-	{
-		if (streq(what, names[i]))
-		{
-			*flags |= (1L << i);
-			return (0);
-		}
-	}
-
-	return (-1);
-}
 
 
 /*
