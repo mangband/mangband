4bed4eb5344836fbc4bb244295bd2553ab919683
jug <jug@802d84f3-b497-254e-bede-06c0a5a3456a>
2008-07-26 15:09:07 +0000

    Player ghosts in walls no longer immune to spells, closes #643.
    Applied patch by powerwyrm.  Added a projectable_wall() function that 
    handles player ghosts correctly.
    
    
    
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@756 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/server/externs.h
+++ src/server/externs.h
@@ -465,6 +465,7 @@ extern void wiz_lite(int Ind);
 extern void wiz_dark(int Ind);
 extern void mmove2(int *y, int *x, int y1, int x1, int y2, int x2);
 extern bool projectable(int Depth, int y1, int x1, int y2, int x2);
+extern bool projectable_wall(int Depth, int y1, int x1, int y2, int x2);
 extern void scatter(int Depth, int *yp, int *xp, int y, int x, int d, int m);
 extern void health_track(int Ind, int m_idx);
 extern void update_health(int m_idx);
