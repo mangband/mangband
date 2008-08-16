440752692779e242b6187ab2f79000bb96738e2d
jug <jug@802d84f3-b497-254e-bede-06c0a5a3456a>
2008-08-16 10:33:33 +0000

    Version bump, closes #674.
    git-svn-id: http://developer.mangband.org/svn/mangband/branches/version-1_1_1@781 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/common/defines.h
+++ src/common/defines.h
@@ -37,7 +37,7 @@
  
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	1
-#define VERSION_PATCH	0
+#define VERSION_PATCH	1
 
 /*
  * This value specifys the suffix to the version info sent to the metaserver.
