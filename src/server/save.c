49eef4b55598d7a54502f9a82620aeb5f20e5d76
jug <jug@802d84f3-b497-254e-bede-06c0a5a3456a>
2008-12-16 16:23:42 +0000

    Port r949 from trunk to stable, addresses #685, closes #762, see #756.
    git-svn-id: http://developer.mangband.org/svn/mangband/branches/version-1_1_2@950 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/server/save.c
+++ src/server/save.c
@@ -1489,7 +1489,7 @@ bool load_server_info(void)
         }
 
 	/* Message */
-	plog(format("Error (%s) reading server savefile.", what));
+	plog(format("Error (%s,%d) reading server savefile.", what, err));
 
 	return (FALSE);
 }
