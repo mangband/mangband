7dca4bcec6ad804fc9fc307a4cd17d822f7b1eb8
flambard <flambard@802d84f3-b497-254e-bede-06c0a5a3456a>
2009-04-09 17:49:33 +0000

    Addresses #832, I forgot to add a file! :(
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@1065 802d84f3-b497-254e-bede-06c0a5a3456a

---
+++ src/server/defines.h
@@ -0,0 +1,56 @@
+/* File: defines.h */
+
+/* Purpose: server-side global constants and macro definitions */
+
+
+#define	OPT_MAX 			34
+#define	MAX_OPTION_GROUPS   4
+
+/*
+ * Available Options
+ *
+ * It is very important that all options stay together
+ * without any gaps, OPT_MAX must not exceed them too.
+ * Order, on the other hand, is not important.
+ *
+ *  *** WRONG ***			*** CORRECT ***
+ * OPT_MAX       64			OPT_MAX       2
+ * OPT_NO_GHOST  5          OPT_NO_GHOST  0
+ * OPT_SUN_LIGHT 9          OPT_SUN_LIGHT 1
+ */
+#define OPT_NO_GHOST    		0
+#define OPT_DEPTH_IN_FEET   	1
+#define OPT_DUNGEON_ALIGN   	2
+#define OPT_AVOID_OTHER 		3
+#define OPT_USE_COLOR   		4
+#define OPT_AUTO_SCUM       	5
+#define OPT_EXPAND_LOOK 		6
+#define OPT_SHOW_DETAILS    	7
+#define OPT_CARRY_QUERY_FLAG	8
+#define OPT_ALWAYS_PICKUP   	9
+#define OPT_USE_OLD_TARGET  	10
+#define OPT_STACK_FORCE_NOTES	11
+#define OPT_STACK_FORCE_COSTS	12
+#define OPT_STACK_ALLOW_ITEMS	13
+#define OPT_STACK_ALLOW_WANDS	14
+#define OPT_FIND_IGNORE_STAIRS	15
+#define OPT_FIND_IGNORE_DOORS	16
+#define OPT_FIND_CUT        	17
+#define OPT_FIND_EXAMINE    	18
+#define OPT_DISTURB_MOVE    	19
+#define OPT_DISTURB_NEAR    	20
+#define OPT_DISTURB_PANEL   	21
+#define OPT_DISTURB_STATE   	22
+#define OPT_DISTURB_MINOR   	23
+#define OPT_DISTURB_OTHER   	24
+#define OPT_VIEW_PERMA_GRIDS	25 
+#define OPT_VIEW_TORCH_GRIDS	26 
+#define OPT_VIEW_REDUCE_LITE	27
+#define OPT_VIEW_REDUCE_VIEW	28
+#define OPT_VIEW_YELLOW_LITE	29
+#define OPT_VIEW_BRIGHT_LITE	30
+#define OPT_VIEW_GRANITE_LITE	31
+#define OPT_VIEW_SPECIAL_LITE	32
+#define OPT_VIEW_ORANGE_LITE	33
+#define OPT_EASY_ALTER			34
+#define option_p(A,B) (A->options[OPT_ ## B])
