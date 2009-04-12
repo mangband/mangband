f17aa44c963fd0d48835490829133ae9e7de30b6
billsey <billsey@802d84f3-b497-254e-bede-06c0a5a3456a>
2009-04-12 18:40:25 +0000

    Fixes #824 - Cleans up a couple more issues where huge variables were being handled as longs instead.
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@1096 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/server/files.c
+++ src/server/files.c
@@ -2821,7 +2821,7 @@ static errr top_twenty(int Ind)
 	the_score.gold[9] = '\0';
 
 	/* Save the current turn */
-	sprintf(the_score.turns, "%9lu", turn);
+	sprintf(the_score.turns, "%9llu", turn);
 	the_score.turns[9] = '\0';
 
 #ifdef HIGHSCORE_DATE_HACK
@@ -2913,7 +2913,7 @@ static errr predict_score(int Ind, int line)
 	sprintf(the_score.gold, "%9lu", (long)p_ptr->au);
 
 	/* Save the current turn */
-	sprintf(the_score.turns, "%9lu", turn);
+	sprintf(the_score.turns, "%9llu", turn);
 
 	/* Hack -- no time needed */
 	strcpy(the_score.day, "TODAY");
