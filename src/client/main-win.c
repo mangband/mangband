a479f39212847aa89ee64959780357b4d65490c8
billsey <billsey@802d84f3-b497-254e-bede-06c0a5a3456a>
2009-04-12 17:22:59 +0000

    Fixes #835 - Implements changes for main-win.c also.
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@1092 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/client/main-win.c
+++ src/client/main-win.c
@@ -944,8 +944,8 @@ static void load_prefs(void)
 	int i;
 #endif
 
-	LPSTR buffer[20] = {'\0'};
-	LPDWORD bufferLen = sizeof(buffer);
+	char buffer[20] = {'\0'};
+	DWORD bufferLen = sizeof(buffer);
 
 #ifdef USE_GRAPHICS
 	/* Extract the "use_graphics" flag */
