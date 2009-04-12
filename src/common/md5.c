65f35d4217f2d3de83593db781a343066efff9d0
flambard <flambard@802d84f3-b497-254e-bede-06c0a5a3456a>
2009-04-12 18:56:42 +0000

    Closes #837, converts pointer before passing it.
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@1098 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/common/md5.c
+++ src/common/md5.c
@@ -340,7 +340,7 @@ extern void MD5Password (char *string)
   char hexval[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
   MD5Init (&context);
-  MD5Update (&context, string, len);
+  MD5Update (&context, (unsigned char*)string, len);
   MD5Final (string, &context);
 
   /* Convert to hex */
