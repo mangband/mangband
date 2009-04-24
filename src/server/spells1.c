26a56c988ee21ed1a138f7d834c43e9d85bddc18
flambard <flambard@802d84f3-b497-254e-bede-06c0a5a3456a>
2009-04-24 09:15:23 +0000

    Port r1123 from trunk to stable, addresses #685.
    git-svn-id: http://developer.mangband.org/svn/mangband/branches/version-1_1_2@1124 802d84f3-b497-254e-bede-06c0a5a3456a

--- src/server/spells1.c
+++ src/server/spells1.c
@@ -3612,6 +3612,10 @@ static bool project_p(int Ind, int who, int r, int Depth, int y, int x, int dam,
 	/* If the player is blind, be more descriptive */
 	if (blind) fuzzy = TRUE;
 
+	/* Mega-Hack */
+	project_m_n++;
+	project_m_x = x;
+	project_m_y = y;
 
 	if (who > 0)
 	{
@@ -4699,7 +4703,7 @@ bool project(int who, int rad, int Depth, int y, int x, int dam, int typ, int fl
 		}
 
 		/* Mega-Hack */
-		if ((who < 0) && (project_m_n == 1))
+		if ((who < 0) && (project_m_n == 1) && !(flg & PROJECT_JUMP))
 		{
 			/* Location */
 			x = project_m_x;
@@ -4716,10 +4720,6 @@ bool project(int who, int rad, int Depth, int y, int x, int dam, int typ, int fl
 				{
 					if (p_ptr->mon_vis[m_idx]) health_track(0 - who, m_idx);
 				}
-				else
-				{
-					if (p_ptr->play_vis[0 - m_idx]) health_track(0 - who, m_idx);
-				}
 			}
 		}
 	}
@@ -4730,6 +4730,11 @@ bool project(int who, int rad, int Depth, int y, int x, int dam, int typ, int fl
 	{
 		/* Start with "dist" of zero */
 		dist = 0;
+
+		/* Mega-Hack */
+		project_m_n = 0;
+		project_m_x = 0;
+		project_m_y = 0;
 
 		/* Now see if the player gets hurt */
 		for (i = 0; i < grids; i++)
@@ -4750,6 +4755,27 @@ bool project(int who, int rad, int Depth, int y, int x, int dam, int typ, int fl
 			/* Affect the player */
 			if (project_p(player_idx, who, dist, Depth, y, x, dam, typ)) notice = TRUE;
 		}
+
+		/* Mega-Hack */
+		if ((who < 0) && (project_m_n == 1) && !(flg & PROJECT_JUMP))
+		{
+			/* Location */
+			x = project_m_x;
+			y = project_m_y;
+
+			/* Still here */
+			if (who < 0)
+			{
+				player_type *p_ptr = Players[0 - who];
+				int m_idx = cave[Depth][y][x].m_idx;
+
+				/* Hack - auto-track player */
+				if (m_idx < 0)
+				{
+					if (p_ptr->play_vis[0 - m_idx]) health_track(0 - who, m_idx);
+				}
+			}
+		}		
 	}
 
 
