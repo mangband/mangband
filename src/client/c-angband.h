e96f82dbc91b1dedc3091552f84bc41ae9dee059
flambard <flambard@802d84f3-b497-254e-bede-06c0a5a3456a>
2009-05-19 21:41:43 +0000

    Closes #76 again, adds files stupidly forgotten from [1190].
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@1191 802d84f3-b497-254e-bede-06c0a5a3456a

---
+++ src/client/c-angband.h
@@ -0,0 +1,9 @@
+/* File: c-angband.h */
+
+/* Client "Angband" header file link */
+
+#ifndef CLIENT
+#define CLIENT
+#endif
+
+#include "../common/angband.h"
---
+++ src/server/mangband.h
@@ -0,0 +1,9 @@
+/* File: mangband.h */
+
+/* Server "Angband" header file link */
+
+#ifndef SERVER
+#define SERVER
+#endif
+
+#include "../common/angband.h"
