11201c88d1292cda5e7f8f697e12a641a8f66483
flambard <flambard@802d84f3-b497-254e-bede-06c0a5a3456a>
2010-03-15 03:25:27 +0000

    Addresses #952, #953, adds forgotten headers.
    git-svn-id: http://developer.mangband.org/svn/mangband/trunk@1378 802d84f3-b497-254e-bede-06c0a5a3456a

---
+++ src/common/net-imps.h
@@ -0,0 +1,53 @@
+#ifndef __NET_IMPLEMENTS_H_
+#define __NET_IMPLEMENTS_H_
+
+#define TV_SEC(A) (A / 1000000)
+#define TV_MSEC(A) (A / 1000)
+#define TV_SET(A,B) {A.tv_sec = 0;A.tv_usec=B;}
+
+/* struct caller_type -- see imps.c */
+typedef struct listener_type listener_type;
+typedef struct connection_type connection_type;
+typedef struct timer_type timer_type;
+struct listener_type {
+	int port;
+	int listen_fd;	
+	callback accept_cb;
+};
+struct connection_type {
+	int conn_fd;
+	callback receive_cb; /* return -1 if you disliked his input */
+	callback close_cb;
+	int close;
+	char host_addr[24];
+	cq rbuf;
+	cq wbuf;
+	data user; /* User-defined data, unused by us */
+};
+struct timer_type {
+	micro interval;
+	micro delay;
+	callback timeout_cb; /* return 1 for infinite, 0 for one-shot, 2 for smooth */ 
+};
+
+extern eptr add_caller(eptr root, char *addr, int port, callback conn_cb, callback fail_cb);
+extern eptr add_listener(eptr root, int port, callback cb);
+extern eptr add_timer(eptr root, int interval, callback timeout);
+extern eptr add_connection(eptr root, int fd, callback read, callback close);
+
+extern eptr handle_listeners(eptr root);
+extern eptr handle_connections(eptr root);
+extern eptr handle_callers(eptr root);
+extern eptr handle_timers(eptr root, long microsec);
+extern long static_timer(int id);
+
+extern void network_reset();
+extern void network_pause(long timeout);
+extern void denaglefd(int fd);
+
+extern int cq_printf(cq *charq, char *str, ...); 
+extern int cq_copyf(cq *src, const char *str, cq *dst);
+extern int cq_scanf(cq *charq, char *str, ...);
+
+extern void e_release_all(eptr node, int d1, int d2);
+#endif
