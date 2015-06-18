7b8f3cc3e63ea238b6375262f7467ceeeb1eef59
flambard-took <flambard@mangband.org>
2015-06-19 01:14:45 +0400

    Address #964, #1031, actually use the new file stream.

--- src/server/cmd4.c
+++ src/server/cmd4.c
@@ -992,7 +992,7 @@ void special_file_peruse(player_type *p_ptr, int type, char query)
 		send_term_info(p_ptr, NTERM_CLEAR | NTERM_ICKY);
 		for (i = 0; i < p_ptr->last_info_line; i++)
 		{
-			Stream_line_p(p_ptr, STREAM_SPECIAL_TEXT, i);
+			Stream_line_p(p_ptr, STREAM_FILE_TEXT, i);
 		}
 		/* Send_term_info(Ind, NTERM_FLUSH, 0); */
 	}
