4a625cb47f3a72dce2aadfad2cc946b8b7d08373
flambard-took <flambard@mangband.org>
2016-03-31 16:08:43 +0300

    Version bump to 1.1.3. See #856.

--- src/common/defines.h
+++ src/common/defines.h
@@ -37,7 +37,7 @@
  
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	1
-#define VERSION_PATCH	2
+#define VERSION_PATCH	3
 
 /*
  * This value specifys the suffix to the version info sent to the metaserver.
