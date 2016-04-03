9e5684dbc63d8b9ddc9b4b8e3b424e9e51547d53
flambard-took <flambard@mangband.org>
2016-04-03 15:03:51 +0400

    Add missing extern for "object_similar_floor", see #993.

--- src/server/externs.h
+++ src/server/externs.h
@@ -783,6 +783,7 @@ extern void object_known(object_type *o_ptr);
 extern void object_aware(player_type *p_ptr, object_type *o_ptr);
 extern void object_tried(int Ind, object_type *o_ptr);
 extern s32b object_value(int Ind, object_type *o_ptr);
+extern bool object_similar_floor(object_type *o_ptr, object_type *j_ptr);
 extern bool object_similar(player_type *p_ptr, object_type *o_ptr, object_type *j_ptr);
 extern void object_absorb(player_type *p_ptr, object_type *o_ptr, object_type *j_ptr);
 extern s16b lookup_kind(int tval, int sval);
