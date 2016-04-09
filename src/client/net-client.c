288ba453c640d3f97bc06e3827e3f440b7043d8c
flambard-took <flambard@mangband.org>
2016-04-09 16:32:18 +0400

    Revert "Address #952, add type declaration to 4 variables in recv_char_info."
    This reverts commit 600eddeea695180a907f4d1472bd1fd3ab724337.
    
    This was caught by evileye, and just shows how stupid flambard is.

--- src/client/net-client.c
+++ src/client/net-client.c
@@ -655,10 +655,9 @@ int recv_play(connection_type *ct) {
 
 /* Character info packet, important at setup stage */
 int recv_char_info(connection_type *ct) {
-	s16b
-		state = 0,
-		race = 0,
-		pclass = 0,
+		state = 0;
+		race = 0;
+		pclass = 0;
 		sex = 0;
 	if (cq_scanf(&ct->rbuf, "%d%d%d%d", &state, &race, &pclass, &sex) < 4)
 	{
