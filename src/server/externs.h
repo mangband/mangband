7d3a63c033026a673ddf4052d0d95ac0d916c945
flambard-took <flambard@mangband.org>
2016-04-16 16:25:25 +0400

    Address #1091, remove "player_name" function from the server.

--- src/server/externs.h
+++ src/server/externs.h
@@ -1160,10 +1160,6 @@ extern bool use_object_current(int Ind);
  * Hack -- conditional (or "bizarre") externs
  */
 
-#ifdef SET_UID
-/* util.c */
-extern void user_name(char *buf, int id);
-#endif
 
 #ifndef HAVE_MEMSET
 /* util.c */
