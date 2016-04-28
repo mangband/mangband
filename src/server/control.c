mangband/mangband

commit f5ccc7848a278acc2b1a73476bb6763492fac377

Address #1121, replace p_ptr->no_ghost checks. Variable dropped.

src/server/control.c
@@ -188,7 +188,7 @@ static void console_who(connection_type* ct, char *useless)
 		player_type *p_ptr = Players[k];
 
 		/* Add an entry */
-		(p_ptr->no_ghost) ? strcpy(brave,"brave \0") : strcpy(brave,"\0"); 
+		if (option_p(p_ptr, NO_GHOST)) strcpy(brave,"brave \0") : strcpy(brave,"\0");
 		cq_printf(&ct->wbuf,"%T", format("%s is a %slevel %d %s %s at %d ft\n", 
 			p_ptr->name, brave, p_ptr->lev, p_name + p_info[p_ptr->prace].name,
 			c_name + c_info[p_ptr->pclass].name, p_ptr->dun_depth*50));
@@ -275,7 +275,7 @@ static void console_whois(connection_type* ct, char *name)
 	/* Output player information */
 
 	/* General character description */
-	(p_ptr->no_ghost) ? strcpy(brave,"brave \0") : strcpy(brave,"\0"); 
+	if (option_p(p_ptr, NO_GHOST)) strcpy(brave,"brave \0") : strcpy(brave,"\0");
 	cq_printf(&ct->wbuf, "%T", format("%s is a %slevel %d %s %s at %d ft\n", 
 		p_ptr->name, brave, p_ptr->lev, p_name + p_info[p_ptr->prace].name,
 		c_name + c_info[p_ptr->pclass].name, p_ptr->dun_depth*50));

src/server/birth.c
@@ -686,7 +686,6 @@ void player_net_wipe(player_type *p_ptr, int reach)
 	p_tmp.cbuf.max = p_ptr->cbuf.max;
 
 	p_tmp.lives = p_ptr->lives;
-	p_tmp.no_ghost = p_ptr->no_ghost; /* ? */
 
 	for (i = 0; i < 6; i++)
 	{
@@ -719,7 +718,8 @@ void player_net_wipe(player_type *p_ptr, int reach)
 	p_ptr->cbuf.max = p_tmp.cbuf.max;
 
 	p_ptr->lives = p_tmp.lives;
-	p_ptr->no_ghost = p_tmp.no_ghost; /* ? */
+
+	/* TODO: also copy p_ptr->options? MAYBE */
 
 	for (i = 0; i < 6; i++)
 	{

src/server/files.c
@@ -2311,7 +2311,7 @@ long total_points(int Ind)
 	player_type *p_ptr = Players[Ind];
 	
 	/* We award a 50% score bonus for bravery with no_ghost characters */
-	if (p_ptr->no_ghost && !cfg_ironman)
+	if (option_p(p_ptr, NO_GHOST) && !cfg_ironman)
 	{
 		return (long)((p_ptr->max_exp + (100 * p_ptr->max_dlv))*1.5);
 	}

src/server/load2.c
@@ -1056,7 +1056,7 @@ static bool rd_extra(player_type *p_ptr, bool had_header)
 
 	if(value_exists("no_ghost"))
 	{
-		p_ptr->no_ghost = read_int("no_ghost");
+		(void)read_int("no_ghost");
 	}
 	
 	p_ptr->max_plv = read_int("max_plv");

src/server/save.c
@@ -474,8 +474,7 @@ static void wr_extra(int Ind)
 	write_int("msp",p_ptr->msp);
 	write_int("csp",p_ptr->csp);
 	write_int("csp_frac",p_ptr->csp_frac);
-	
-	write_int("no_ghost",p_ptr->no_ghost);
+
 
 	/* Max Player and Dungeon Levels */
 	write_int("max_plv",p_ptr->max_plv);

src/server/xtra2.c
@@ -2813,7 +2813,7 @@ void player_death(int Ind)
 	player_strip(Ind, TRUE, TRUE, TRUE, TRUE);
 
 	/* Last chance to survive death: */
-	if (cfg_ironman || p_ptr->no_ghost)
+	if (cfg_ironman || option_p(p_ptr, NO_GHOST))
 	{
 		/* Get rid of him */
 		player_funeral(Ind, format("Killed by %s", p_ptr->died_from));

commit b1148909243c4ddeb752b44e4b456995e9e5aad3

Fix build broken in f5ccc7848a278acc2b1a73476bb6763492fac377.
See #1121.

src/server/control.c
@@ -188,7 +188,7 @@ static void console_who(connection_type* ct, char *useless)
 		player_type *p_ptr = Players[k];
 
 		/* Add an entry */
-		if (option_p(p_ptr, NO_GHOST)) strcpy(brave,"brave \0") : strcpy(brave,"\0");
+		if (option_p(p_ptr, NO_GHOST)) strcpy(brave,"brave \0"); else strcpy(brave,"\0");
 		cq_printf(&ct->wbuf,"%T", format("%s is a %slevel %d %s %s at %d ft\n", 
 			p_ptr->name, brave, p_ptr->lev, p_name + p_info[p_ptr->prace].name,
 			c_name + c_info[p_ptr->pclass].name, p_ptr->dun_depth*50));
@@ -275,7 +275,7 @@ static void console_whois(connection_type* ct, char *name)
 	/* Output player information */
 
 	/* General character description */
-	if (option_p(p_ptr, NO_GHOST)) strcpy(brave,"brave \0") : strcpy(brave,"\0");
+	if (option_p(p_ptr, NO_GHOST)) strcpy(brave,"brave \0"); else strcpy(brave,"\0");
 	cq_printf(&ct->wbuf, "%T", format("%s is a %slevel %d %s %s at %d ft\n", 
 		p_ptr->name, brave, p_ptr->lev, p_name + p_info[p_ptr->prace].name,
 		c_name + c_info[p_ptr->pclass].name, p_ptr->dun_depth*50));