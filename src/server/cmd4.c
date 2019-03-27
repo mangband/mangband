mangband/mangband

commit ff3ca409ef6a7cfe2a12108a60e16bd99358310c

Address #670, make sure monster recall is resent.
It was still possible to get some garbage after using non-monster-recall
SPECIAL stream.

src/server/cmd4.c
@@ -1244,4 +1244,7 @@ void do_cmd_interactive(player_type *p_ptr, char query)
 
 	/* Hack -- return to main term */
 	send_term_info(p_ptr, NTERM_ACTIVATE, NTERM_WIN_OVERHEAD);
+
+	/* Hack -- cancel monster tracking (maybe) */
+	monster_race_track_hack(Get_Ind[p_ptr->conn]);
 }

src/server/externs.h
@@ -1031,6 +1031,7 @@ extern int color_opposite(int color);
 extern cptr attr_to_text(byte a);
 extern void send_prepared_info(player_type *p_ptr, byte win, byte stream, byte extra_params);
 extern void send_prepared_popup(int Ind, cptr header);
+extern void monster_race_track_hack(int Ind);
 extern void text_out(cptr buf);
 extern void text_out_c(byte a, cptr buf);
 extern void text_out_init(int Ind);

src/server/net-game.c
@@ -1293,6 +1293,8 @@ int recv_stream_size(connection_type *ct, player_type *p_ptr) {
 		else
 		{
 			p_ptr->window_flag &= ~streams[st].window_flag;
+			/* HACK -- check if player has disabled monster text window */
+			monster_race_track_hack(Ind);
 		}
 	}
 

src/server/util.c
@@ -3571,6 +3571,24 @@ void send_prepared_popup(int Ind, cptr header)
 	send_term_info(p_ptr, NTERM_POP, 0);
 
 	send_term_info(p_ptr, NTERM_ACTIVATE, old_term);
+
+	/* HACK -- Assume this was NOT monster recall */
+	/* This is implied, because monster recall doesn't use send_prepared_popup() */
+	monster_race_track_hack(Ind);
+}
+
+/* This hacky function resets monster tracking after STREAM_SPECIAL_TEXT
+ * was used for anything other than actual monster recall. This way,
+ * server will definitely send new monster info, once it's required again. */
+void monster_race_track_hack(int Ind)
+{
+	player_type *p_ptr = Players[Ind];
+	/* Only relevant if Player has no dedicated window for monster text */
+	if (!p_ptr->stream_wid[STREAM_MONSTER_TEXT])
+	{
+		/* Hack -- cancel monster tracking */
+		monster_race_track(Ind, -1);
+	}
 }
 
 void text_out_init(int Ind) {

commit 8cdf6df98d5cc3d20a6dcf9b8954f7c1917a2df8

Address #670, fix crash introduced in ff3ca409ef6a.

src/server/cmd4.c
@@ -1246,5 +1246,5 @@ void do_cmd_interactive(player_type *p_ptr, char query)
 	send_term_info(p_ptr, NTERM_ACTIVATE, NTERM_WIN_OVERHEAD);
 
 	/* Hack -- cancel monster tracking (maybe) */
-	monster_race_track_hack(Get_Ind[p_ptr->conn]);
+	monster_race_track_hack(p_ptr);
 }

src/server/externs.h
@@ -1033,7 +1033,7 @@ extern int color_opposite(int color);
 extern cptr attr_to_text(byte a);
 extern void send_prepared_info(player_type *p_ptr, byte win, byte stream, byte extra_params);
 extern void send_prepared_popup(int Ind, cptr header);
-extern void monster_race_track_hack(int Ind);
+extern void monster_race_track_hack(player_type *p_ptr);
 extern void text_out(cptr buf);
 extern void text_out_c(byte a, cptr buf);
 extern void text_out_init(int Ind);

src/server/net-game.c
@@ -1333,7 +1333,7 @@ int recv_stream_size(connection_type *ct, player_type *p_ptr) {
 		{
 			p_ptr->window_flag &= ~streams[st].window_flag;
 			/* HACK -- check if player has disabled monster text window */
-			monster_race_track_hack(Ind);
+			monster_race_track_hack(p_ptr);
 		}
 	}
 

src/server/util.c
@@ -3573,16 +3573,17 @@ void send_prepared_popup(int Ind, cptr header)
 
 	/* HACK -- Assume this was NOT monster recall */
 	/* This is implied, because monster recall doesn't use send_prepared_popup() */
-	monster_race_track_hack(Ind);
+	monster_race_track_hack(p_ptr);
 }
 
 /* This hacky function resets monster tracking after STREAM_SPECIAL_TEXT
  * was used for anything other than actual monster recall. This way,
  * server will definitely send new monster info, once it's required again. */
-void monster_race_track_hack(int Ind)
+void monster_race_track_hack(player_type *p_ptr)
 {
-	player_type *p_ptr = Players[Ind];
-	if (!Ind) return;
+	int Ind = Get_Ind[p_ptr->conn];
+	/* Paranoia -- Player is not yet in the game */
+	if (Ind < 1) return;
 	/* Only relevant if Player has no dedicated window for monster text */
 	if (!p_ptr->stream_wid[STREAM_MONSTER_TEXT])
 	{