7ea3811a93dda45f92113186d7eef8c36b6d72c0
flambard-took <flambard@mangband.org>
2019-03-28 18:14:54 +0000

    Address #1225, add docker script to build many linux packages.

---
+++ dist/docker/build.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# This script does the massive build. Scroll down to see
+# "build_it" invocations, which determine the actual build targets.
+# The only argument to this script is optional "--flat", to force
+# everything into a single directory. (Useful for github releases)
+# Note: this can hours!
+
+## NON-DOCKER PATH
+running_in_docker() {
+    (awk -F/ '$2 == "docker"' /proc/self/cgroup | read non_empty_input)
+}
+build_it() {
+    IMAGE=$1 ; BUILD_MODE=$2 ; TARGET_DIR=$3 ; GLOB=$4 ; FLATMODE=$5
+    echo "Run docker with image {$IMAGE} to generate ${GLOB} files"
+    rm -rf builds
+    mkdir -p builds
+    docker pull ${IMAGE}
+    docker run -v`pwd`/../../:/io -w /io ${IMAGE} /io/dist/docker/build.sh ${BUILD_MODE}
+    if [ "$?" = "0" ]; then
+        if [ "$FLATMODE" = "--flat" ]; then
+            # copy all made files into a SINGLE dir, but with a prefix
+            find ./builds -name "${GLOB}" | while read file
+            do
+                FNAME=$(basename $file)
+                FNAME=${FNAME/-1.x86/.x86} # remove "-1" release tag
+                FNAME=${FNAME/-1.amd/.amd} # hacky, but should work
+                DP=(${TARGET_DIR//-/ })
+                echo " * ${FNAME} ${DP[0]} ( ${DP[1]} ) "
+                cp $file flat/${DP[0]}_$FNAME
+            done
+        else
+            # copy all made files into target dir
+            mkdir -p ${TARGET_DIR}
+            cp builds/${GLOB} ${TARGET_DIR}/.
+        fi
+    fi
+}
+if ! running_in_docker ; then
+    if [ "$1" = "--flat" ]; then
+        rm -rf flat
+        mkdir -p flat
+    fi
+
+    # make dist once
+#    echo "Running initial make dist"
+#    cd ../..
+#    ./autogen.sh -n
+#    ./configure
+#    cd dist/tgz
+#    ./package.sh
+#    cd ../../dist/docker
+
+    ################# ******************** #################
+
+    build_it "debian:stretch" "DEB" "debian9-amd64" "*.deb" $1
+    build_it "i386/debian:stretch" "DEB" "debian9-i386" "*.deb" $1
+    build_it "debian:jessie" "DEB" "debian8-amd64" "*.deb" $1
+    build_it "i386/debian:jessie" "DEB" "debian8-i386" "*.deb" $1
+
+    build_it "ubuntu:bionic" "DEB" "ubuntu18-amd64" "*.deb" $1
+    build_it "i386/ubuntu:bionic" "DEB" "ubuntu18-i386" "*.deb" $1
+    build_it "ubuntu:disco" "DEB" "ubuntu19-amd64" "*.deb" $1
+    build_it "i386/ubuntu:disco" "DEB" "ubuntu19-i386" "*.deb" $1
+    build_it "ubuntu:trusty" "DEB" "ubuntu14-amd64" "*.deb" $1
+    build_it "i386/ubuntu:trusty" "DEB" "ubuntu14-i386" "*.deb" $1
+    build_it "ubuntu:xenial" "DEB" "ubuntu16-amd64" "*.deb" $1
+    build_it "i386/ubuntu:xenial" "DEB" "ubuntu16-i386" "*.deb" $1
+
+    build_it "centos:7" "RPM" "centos7-amd64" "*.rpm" $1
+    build_it "i386/centos:7" "RPM" "centos7-i386" "*.rpm" $1
+    build_it "centos:6" "RPM" "centos6-amd64" "*.rpm" $1
+# broken, for some reason
+#    build_it "i386/centos:6" "RPM" "centos6-i386" "*.rpm" $1
+
+
+    ################# ******************** #################
+    exit
+fi
+
+## DOCKER PATH START IS HERE
+set -e -x
+
+if [ $1 = "DEB" ]; then
+	printf "\n *** Doing debian builds *** \n\n"
+	apt-get update
+	apt-get install -y make gcc autoconf automake dh-make
+	apt-get install -y dpkg-dev # needed on Ubuntu14
+	apt-get install -y libncurses5-dev libsdl1.2-dev
+	apt-get install -y libX11-dev || apt-get install -y libx11-dev
+# make dist
+#	ls
+#	./autogen.sh -n
+#	./configure
+#	cd dist/tgz
+#	./package.sh
+#	cd -
+	cd dist/deb
+	rm -f *.deb
+	export USER=mangband
+	./debit.sh gcu
+	./debit.sh sdl
+	./debit.sh x11
+	cd -
+	cp dist/deb/*.deb dist/docker/builds/.
+fi
+
+if [ $1 = "RPM" ]; then
+	printf "\n *** Doing rpm builds *** \n\n"
+	yum install -y wget which make gcc automake autoconf rpmdevtools
+	yum install -y ncurses-devel libX11-devel SDL-devel
+# make dist
+#	ls
+#	./autogen.sh -n
+#	./configure
+#	cd dist/tgz
+#	./package.sh
+#	cd -
+	cd dist/rpm
+	echo "%packager MAngband Project Team <team@mangband.org>" >> /root/.rpmmacros
+	echo "%vendor   MAngabnd Project Team" >> /root/.rpmmacros
+	echo "%_topdir  /io/dist/rpm" >> /root/.rpmmacros
+	./rpmit.sh
+	cd -
+	cp dist/rpm/RPMS/**/*.rpm dist/docker/builds/.
+	#cp dist/rpm/SRPMS/**/.rpm dist/docker/builds/.
+fi
