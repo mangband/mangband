8e8ad7305ffc3d3f5bec8d01125d0f47810c36b6
flambard-took <flambard@mangband.org>
2019-04-18 08:09:18 +0000

    Close #1245, save birth options after character creation.

--- src/client/c-init.c
+++ src/client/c-init.c
@@ -594,6 +594,9 @@ bool client_setup()
 	/* Initialize the pref files */
 	initialize_all_pref_files();
 
+	/* Horrible hack -- resave birth options if player adjusted them */
+	if (ignore_birth_options) Save_options();
+
 	/* Send request for MOTD to read (optional) */
 	//Send_motd(0); // pass -1 to receive motd off-screen
 
