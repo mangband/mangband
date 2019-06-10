b913d10a326c5d659a32e834e1c3529d80c151f5
flambard-took <flambard@mangband.org>
2019-06-10 17:20:28 +0000

    Address #1295, add a hack to allow SDL2 rerender in tight spots.

--- src/client/net-client.c
+++ src/client/net-client.c
@@ -2104,6 +2104,8 @@ int call_metaserver(char *server_name, int server_port, char *buf, int buflen)
 		network_pause(100000); /* 0.1 ms "sleep" */
 		/* Let windows process UI events: */
 		Term_xtra(TERM_XTRA_FLUSH, 0);
+		/* Let SDL2 client re-render: */
+		Term_xtra(TERM_XTRA_BORED, 0);
 	}
 	/* Will be either 1 either -1 */
 
