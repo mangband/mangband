mangband/mangband

commit b7cfbed4069fbe931ea9e727699ff3e8d329ab65

Close #1342, add --help, --version and -m switches to mangclient.

src/client/client.c
@@ -69,21 +69,144 @@ static void read_credentials(void)
 		my_strcpy(real_name, buffer, MAX_CHARS);
 	}
 #endif
-
 }
+
+/*
+ * --help, --version and main()
+ */
+const static char frontends[][8] = {
+#ifdef USE_SDL
+	"sdl",
+#endif
+#ifdef USE_SDL2
+	"sdl2",
+#endif
+#ifdef USE_X11
+	"x11",
+#endif
+#ifdef USE_GCU
+	"gcu",
+#endif
+};
+static int num_frontends = sizeof(frontends) / 8;
+static void show_version()
+{
+	int i;
+	printf("mangclient (MAngband Client) %d.%d.%d",
+		CLIENT_VERSION_MAJOR, CLIENT_VERSION_MINOR, CLIENT_VERSION_PATCH);
+	if (CLIENT_VERSION_EXTRA == 1) printf("alpha");
+	else if (CLIENT_VERSION_EXTRA == 2) printf("beta");
+	else if (CLIENT_VERSION_EXTRA == 3) printf("devel");
+	printf("\n");
+	printf("Copyright (C) 2011-2019 MAngband Project Team.\n");
+	printf("License: MAngband license, see <LICENSE> file.\n");
+	printf("Compiled with ");
+	for (i = 0; i < num_frontends; i++)
+	{
+		if (i) printf("%s", i == num_frontends - 1 ? " and " : ", ");
+		printf("'%s'", frontends[i]);
+	}
+	if (!i) printf("no(!)");
+	printf(" display modules.\n");
+}
+static void show_help()
+{
+	int i;
+	printf("Usage: %s [OPTIONS] [SERVER [PORT]]\n", argv0);
+	printf("\n");
+	printf("SERVER can be a hostname or an IPv4 address. PORT can also be supplied.\n");
+	printf("If SERVER is not specified, %s will try to find available servers via the metaserver.\n", argv0);
+	printf("\n");
+	printf("Options\n");
+	printf("  -mDISPLAY                 Pick display module. \n");
+	printf("                            Available modules are ");
+	for (i = 0; i < num_frontends; i++)
+	{
+		if (i) printf("%s", i == num_frontends - 1 ? " and " : ", ");
+		printf("'%s'", frontends[i]);
+	}
+	if (!i) printf(" ..NONE..");
+	printf(".\n");
+	printf("      --config PATH         Config file location.\n");
+	printf("      --libdir PATH         Readable asset dir location.\n");
+	printf("      --userdir PATH        Writable user dir location.\n");
+	printf("      --nick NICKNAME       Character name to use.\n");
+	printf("\nDisplay modules might support additional arguments.\n");
+#ifdef USE_SDL
+	extern const char help_sdl[];
+	printf("%s", help_sdl);
+#endif
+#ifdef USE_SDL2
+	extern const char help_sdl2[];
+	printf("%s", help_sdl2);
+#endif
+#ifdef USE_X11
+	extern const char help_x11[];
+	printf("%s", help_x11);
+#endif
+#ifdef USE_GCU
+	extern const char help_gcu[];
+	printf("%s", help_gcu);
+#endif
+}
+static bool exit_promptly(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "--version"))
+		{
+			show_version();
+			return TRUE;
+		}
+		else if (!strcmp(argv[i], "--help"))
+		{
+			show_help();
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+/* FIXME: This "parser" is horrible */
+static void pick_module(char *buf, size_t len, int argc, char *argv[])
+{
+	int i, j;
+	for (i = 0; i < num_frontends; i++)
+	{
+		char tmp[32];
+		sprintf(tmp, "-m%s", frontends[i]);
+		for (j = 0; j < argc; j++)
+		{
+			if(!strcmp(argv[j], tmp))
+			{
+				strcpy(argv[j], "");/* <- probably illegal */
+				my_strcpy(buf, frontends[i], len);
+				break;
+			}
+		}
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
+	char prefer_module[16] = { 0 };
 	bool done = FALSE;
 
 	/* Save the program name */
 	argv0 = argv[0];
+
+	/* Do --version and --help */
+	if (exit_promptly(argc, argv)) return 0;
 
 	/* Save command-line arguments */
 	clia_init(argc, (const char**)argv);
 
 	/* Client Config-file */
 	conf_init(NULL);
+
+	/* Pick preferred module */
+	pick_module(prefer_module, sizeof(prefer_module), argc, argv);
 
 #ifdef __APPLE__
 	// Set our "cwd" to the directory the application bundle is in on OS X
@@ -100,7 +223,7 @@ int main(int argc, char *argv[])
 	/* Attempt to initialize a visual module */
 #ifdef USE_SDL
 	/* Attempt to use the "main-sdl.c" support */
-	if (!done)
+	if (!done && (!strcmp(prefer_module, "sdl") || STRZERO(prefer_module)))
 	{
 		extern errr init_sdl(void);
 		if (0 == init_sdl()) done = TRUE;
@@ -108,7 +231,7 @@ int main(int argc, char *argv[])
 	}
 #endif
 #ifdef USE_SDL2
-	if (!done)
+	if (!done && (!strcmp(prefer_module, "sdl2") || STRZERO(prefer_module)))
 	{
 		extern errr init_sdl2(void);
 		if (init_sdl2() == 0) done = TRUE;
@@ -129,7 +252,7 @@ int main(int argc, char *argv[])
 
 #ifdef USE_X11
 	/* Attempt to use the "main-x11.c" support */
-	if (!done)
+	if (!done && (!strcmp(prefer_module, "x11") || STRZERO(prefer_module)))
 	{
 		extern errr init_x11(int argc, char **argv);
 		if (0 == init_x11(argc,argv)) done = TRUE;
@@ -139,7 +262,7 @@ int main(int argc, char *argv[])
 
 #ifdef USE_GCU
 	/* Attempt to use the "main-gcu.c" support */
-	if (!done)
+	if (!done && (!strcmp(prefer_module, "gcu") || STRZERO(prefer_module)))
 	{
 		extern errr init_gcu(void);
 		if (0 == init_gcu()) done = TRUE;

src/client/main-gcu.c
@@ -954,6 +954,8 @@ static errr term_data_init(term_data *td, int rows, int cols, int y, int x)
 	return (0);
 }
 
+const char help_gcu[] = "";
+
 /*
  * Prepare "curses" for use by the file "term.c"
  *

src/client/main-sdl.c
@@ -2529,6 +2529,17 @@ static void quit_sdl(cptr str)
 	SDL_Quit();
 }
 
+/* "--help" text */
+const char help_sdl[] =
+"SDL module (single-window client):\n"
+"      --width PIXELS        Window width.\n"
+"      --height PIXELS       Window height.\n"
+"      --bpp BPP             Bits-Per-Pixel, 32 by default.\n"
+"      --fullscreen          Run in fullscreen mode.\n"
+"      --graphics TILESET    Tileset ID to use, 0 to disable graphics.\n"
+"      --sound TOGGLE        0 to disable sound, 1 to enable.\n"
+;
+
 /*
  * A "normal" system uses "main.c" for the "main()" function, and
  * simply adds a call to "init_xxx()" to that function, conditional

src/client/main-sdl2.c
@@ -199,15 +199,16 @@ static void mobileAutoLayout(void)
 	refreshTerm(td);
 }
 
+/* ==== Help ==== */
+const char help_sdl2[] = "";
+
 /* ==== Initialize function ==== */
 /* init_sdl2
 Our initializer function. Sets up SDL2 and creates our z-terms (windows).
 */
 errr init_sdl2(void) {
 	char buf[1024];
-	 int i;
-
-	init_stuff(); // load in paths
+	int i;
 
 	// **** Load in Configuration ****
 	// The following global vars are set AFTER init_sdl2(), but as per below, we need 'em

commit bf1fab35e7f84f69d802fe78c986cd7a0fdecbb4

Address #1342, fix VS builds, broken in b7cfbed.

src/client/client.c
@@ -133,20 +133,20 @@ static void show_help()
 	printf("      --nick NICKNAME       Character name to use.\n");
 	printf("\nDisplay modules might support additional arguments.\n");
 #ifdef USE_SDL
-	extern const char help_sdl[];
-	printf("%s", help_sdl);
+	{ extern const char help_sdl[];
+	printf("%s", help_sdl); }
 #endif
 #ifdef USE_SDL2
-	extern const char help_sdl2[];
-	printf("%s", help_sdl2);
+	{ extern const char help_sdl2[];
+	printf("%s", help_sdl2); }
 #endif
 #ifdef USE_X11
-	extern const char help_x11[];
-	printf("%s", help_x11);
+	{ extern const char help_x11[];
+	printf("%s", help_x11); }
 #endif
 #ifdef USE_GCU
-	extern const char help_gcu[];
-	printf("%s", help_gcu);
+	{ extern const char help_gcu[];
+	printf("%s", help_gcu); }
 #endif
 }
 static bool exit_promptly(int argc, char *argv[])