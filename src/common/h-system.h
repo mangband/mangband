30751d0fc3c8b60723b2c19133afa85a1430fd79
flambard-took <flambard@mangband.org>
2019-08-29 14:43:41 +0000

    Address #1348, include winsock earlier, so we can have select-as-usleep.
    I liked it more when winsock was included in net-imps, oh well.

--- src/common/h-system.h
+++ src/common/h-system.h
@@ -31,7 +31,10 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+/* define WIN_VER here? */
 #include "windows.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #endif
 
 #ifdef SET_UID
